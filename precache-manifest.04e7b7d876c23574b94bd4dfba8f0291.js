ea52110fca06e9053f9e31a3afdf199a9b643d3d
Nikeeta Goyal <NikeetaG@quinnox.com>
2019-12-18 15:56:51 +0530

    Updates

---
+++ precache-manifest.04e7b7d876c23574b94bd4dfba8f0291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a65f0ee47f5ee16ed68eca330e2596",
+    "url": "/visibilitydashboard/index.html"
+  },
+  {
+    "revision": "90f9c0c8b700adeb8262",
+    "url": "/visibilitydashboard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a8f2df6bdd5983c5a8c",
+    "url": "/visibilitydashboard/static/js/2.ba9a093b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/visibilitydashboard/static/js/2.ba9a093b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90f9c0c8b700adeb8262",
+    "url": "/visibilitydashboard/static/js/main.a07157dc.chunk.js"
+  },
+  {
+    "revision": "d6dcc15cdbc9dd4e3cde",
+    "url": "/visibilitydashboard/static/js/runtime-main.394e2ea6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/visibilitydashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
